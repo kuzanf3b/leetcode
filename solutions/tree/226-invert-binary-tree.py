71cc5a709fad62c8b082e8b897364b2b2677154d
fzn <fauzn88br@gmail.com>
2026-09-20 21:32:22 +0700

    refactor: 226. Invert Binary Tree
    Make it the swaping node logic to one-liners code

--- solutions/tree/226-invert-binary-tree.py
+++ solutions/tree/226-invert-binary-tree.py
@@ -9,11 +9,13 @@ class Solution:
         if root is None:
             return None
 
-        temp = root.left
-        root.left = root.right
-        root.right = temp
+        # temp = root.left
+        # root.left = root.right
+        # root.right = temp
+        #
+        # self.invertTree(root.left)
+        # self.invertTree(root.right)
 
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
 
         return root
