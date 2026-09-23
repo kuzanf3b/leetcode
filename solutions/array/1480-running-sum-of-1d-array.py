c9f76c3d606d5b96b4d797990848988520f18b3e
fzn <fauzn88br@gmail.com>
2026-09-23 12:31:26 +0700

    refactor: 1480. Running Sum of 1d Array
    Remove the copy of nums array and make the iterate more simpler

--- solutions/array/1480-running-sum-of-1d-array.py
+++ solutions/array/1480-running-sum-of-1d-array.py
@@ -1,12 +1,9 @@
 class Solution:
     def runningSum(self, nums: list[int]) -> list[int]:
-        results = [0] * len(nums)
-        results[0] = nums[0]
-
         for num in range(1, len(nums)):
-            results[num] = nums[num] + results[num - 1]
+            nums[num] = nums[num - 1]
 
-        return results
+        return nums
 
     # time complexity: O(n)
     # space complexity: O(1)
