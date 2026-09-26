0064d13046c9746967d26c6513a760aaef059169
fzn <fauzn88br@gmail.com>
2026-09-26 20:59:41 +0700

    feat: 20 Valid Parentheses
    Add time and space complexity explanation for this problem

--- stack/20-valid-parentheses.py
+++ stack/20-valid-parentheses.py
@@ -11,3 +11,6 @@ class Solution:
                 stack.append(char)
                 
         return not stack
+
+    # time complexity: O(n), where n is the length of the input string s. We iterate through each character in the string once, performing constant-time operations for each character.
+    # space complexity: O(n), where n is the length of the input string s. In the worst case, we may need to store all opening brackets in the stack, which can take up to O(n) space.
